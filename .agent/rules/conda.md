98d6d37ba859844a8ecb6f248076cbd0d2e651e4
JettyCoffee <Jetty_Coffee@qq.com>
2025-11-19 23:54:53 +0800

    使用新方法构建

---
+++ .agent/rules/conda.md
@@ -0,0 +1,5 @@
+---
+trigger: always_on
+---
+
+using conda activate rolerag before any terminal command to make sure you are in right env
--- downloads.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("shubhammaindola/harry-potter-books")
-
-print("Path to dataset files:", path)
